9321db4a7772a3c9bdc7dc9b8858b12d21512947
thierry <thierry_192@outlook.com>
2020-05-25 01:48:52 -0300

    core ready

--- data/data.js
+++ data/data.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const mysqlConnection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'gabone28',
+  password: 'your password',
   database: 'test'
 });
 
