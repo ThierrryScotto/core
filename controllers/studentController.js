b3c181d30246a2bf1e99f0315fe00debef95b6dc
thierry <thierry_192@outlook.com>
2020-05-26 01:24:24 -0300

    fix  return and object message

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,22 +3,22 @@ const mysqlConnection = require("../data/data");
 module.exports = {
   getAllStudents(req, res) {
     mysqlConnection.query('SELECT * FROM Students', (error, results, fields )=> {
-      if (error) res.send({ error });
-      res.send(results);
+      if (error) res.send({success: false, error: "Erro ao buscar usuários"})
+      res.send({success: true, users: results});
     });
   },
   getOneStudent(req, res) {
     mysqlConnection.query('SELECT * FROM Students WHERE register = ?',[req.params.id],
     (error, results, fields )=> {
-      if (error) res.send({ error });
-      res.send(results[0]);
+      if (error) res.send({success: false, error: "Erro ao buscar usuário"})
+      res.send({ success: true, user: results[0]});
     });
   },
   deleteStudent(req, res) {
     mysqlConnection.query('DELETE FROM Students WHERE register = ?',[req.params.id],
     (error, results, fields )=> {
-      if (error) res.send({ error });
-      res.send(results);
+      if (error) res.send({success: false, error: "Erro ao deletar usuário"})
+      res.send({success: true});
     });
   },
   editStundent(req, res) {
@@ -28,8 +28,8 @@ module.exports = {
     WHERE register = ${Number(register)};`;
 
     mysqlConnection.query(sqlCommand,(error, results, fields )=> {
-      if (error) res.send({ error });
-      res.send({success: "Usuário editado com sucesso"});
+      if (error) res.send({success: false, error: "Erro ao editar usuário"})
+      res.send({success: true});
     });
   }, 
   registerStudent(req, res) {
