c636e0da8631f07d9b95d663f962f872bb5f2e9a
thierry <thierry_192@outlook.com>
2020-05-26 22:57:37 -0300

    fix env

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,21 +1,22 @@
 const mysqlConnection = require("../data/data");
+require('dotenv/config');
 
 module.exports = {
   getAllStudents(req, res) {
-    mysqlConnection.query('SELECT * FROM Students', (error, results, fields )=> {
+    mysqlConnection.query(`SELECT * FROM ${process.env.DB_TABLE}`, (error, results, fields )=> {
       if (error) res.send({success: false, error: "Erro ao buscar usuários"})
       res.send({success: true, users: results});
     });
   },
   getOneStudent(req, res) {
-    mysqlConnection.query('SELECT * FROM Students WHERE register = ?',[req.params.id],
+    mysqlConnection.query(`SELECT * FROM ${process.env.DB_TABLE} WHERE register = ?`,[req.params.id],
     (error, results, fields )=> {
       if (error) res.send({success: false, error: "Erro ao buscar usuário"})
       res.send({ success: true, user: results[0]});
     });
   },
   deleteStudent(req, res) {
-    mysqlConnection.query('DELETE FROM Students WHERE register = ?',[req.params.id],
+    mysqlConnection.query(`DELETE FROM ${process.env.DB_TABLE} WHERE register = ?`,[req.params.id],
     (error, results, fields )=> {
       if (error) res.send({success: false, error: "Erro ao deletar usuário"})
       res.send({success: true});
@@ -23,7 +24,7 @@ module.exports = {
   },
   editStundent(req, res) {
     let { register, name, lastName, email, sex } = req.body.student;
-    let sqlCommand = `UPDATE Students
+    let sqlCommand = `UPDATE ${process.env.DB_TABLE}
     SET name = "${name}", lastName = "${lastName}", email = "${email}", sex = "${sex}"
     WHERE register = ${Number(register)};`;
 
@@ -38,7 +39,7 @@ module.exports = {
     if (!student) 
       res.send({ error: "Houve algum erro"})
 
-    let sqlCommand = `INSERT INTO Students (name, email, document, lastName, sex) VALUES
+    let sqlCommand = `INSERT INTO ${process.env.DB_TABLE} (name, email, document, lastName, sex) VALUES
      ("${student.name}","${student.email}", "${student.document}", "${student.lastName}", "${student.sex}");`
 
     mysqlConnection.query(sqlCommand,(error, results, fields )=> {
